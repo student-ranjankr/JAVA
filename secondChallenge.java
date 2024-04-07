ca2947956046eeaa81839463f66be79057f5bec7
student-ranjankr <165934316+student-ranjankr@users.noreply.github.com>
2024-04-07 22:03:46 +0530

    Add files via upload

---
+++ secondChallenge.java
@@ -0,0 +1,16 @@
+package Challenge2;
+
+import java.util.Scanner;
+
+public class secondChallenge {
+    public static void main(String[] args) {
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter The First Number :- ");
+        int a = sc.nextInt();
+        System.out.println(" Enter The Second Number :- ");
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println(" The Sum of two numbers is :- \n"+c);
+    }
+}
