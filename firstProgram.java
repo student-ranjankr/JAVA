88b382eda6a1b1b40cb2b756e04fb35cfc389b39
student-ranjankr <165934316+student-ranjankr@users.noreply.github.com>
2024-04-10 23:29:17 +0530

    Add files via upload

---
+++ firstProgram.java
@@ -0,0 +1,16 @@
+package TypeConversion;
+public class firstProgram
+{
+    public static void main(String[] args)
+    {
+        System.out.println();
+        float a = 4;
+        System.out.println(a);
+
+        int b = (int)5.89f;
+        System.out.println(b);
+
+        int c = 5;
+        System.out.println((double)5);
+    }
+}
