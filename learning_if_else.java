9b2d41f8406eb67c38669d72fc98762331ce03a9
student-ranjankr <165934316+student-ranjankr@users.noreply.github.com>
2024-04-11 19:29:13 +0530

    Add files via upload

---
+++ learning_if_else.java
@@ -0,0 +1,21 @@
+package if_else;
+
+public class learning_if_else {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println(" learning_if_else ");
+        System.out.println();
+        boolean isMale = true;
+        String name = "Bob";
+        System.out.println("before if");
+        if(isMale)
+        {
+            System.out.println("Mr."+name);
+        }
+        else
+        {
+            System.out.println("Mrs."+name);
+        }
+        System.out.println("after if");
+    }
+}
