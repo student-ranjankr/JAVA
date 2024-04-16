29687364885bcaab088b2f434ff155a442cbd7b6
student-ranjankr <165934316+student-ranjankr@users.noreply.github.com>
2024-04-17 02:01:38 +0530

    Add files via upload

---
+++ patterns_.java
@@ -0,0 +1,18 @@
+package Challenge8;
+
+import java.util.Scanner;
+
+public class patterns_
+{
+    public static void main(String[] args)
+    {
+        System.out.println();
+        System.out.println(" Welcome to our pattern problems : ");
+        System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" Please enter the no. of rows :- ");
+        int rows = sc.nextInt();
+
+    }
+
+}
