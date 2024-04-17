2f0d88f8dd5ff2b77f2e4d694d63211eee91648d
student-ranjankr <165934316+student-ranjankr@users.noreply.github.com>
2024-04-18 00:48:12 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,37 @@
+package datatypes_variables_input;
+
+public class Variables
+{
+    public static void main(String[] args)
+    {
+        System.out.println();
+      int a = 5;
+      int b =7;
+        System.out.println(a);
+        System.out.println(b);
+        int c;
+        c=90;
+        System.out.println(c);
+        c = 89;
+        System.out.println(c);
+
+        boolean isVegetarian = true;
+        System.out.println(isVegetarian);
+
+        float myFloat = 5.6f;
+        System.out.println(myFloat);
+
+
+        double myDouble = 6.8;
+        System.out.println(myDouble);
+
+        String str = "Ranjan";
+        System.out.println(str);
+
+
+        char myChar = 'A';
+        System.out.println(myChar);
+
+
+    }
+}
