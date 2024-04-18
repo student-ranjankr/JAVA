4aaec1c491d3b73e7a7ee759df90a46eb01708eb
student-ranjankr <165934316+student-ranjankr@users.noreply.github.com>
2024-04-19 02:13:41 +0530

    Add files via upload

---
+++ Escape.java
@@ -0,0 +1,8 @@
+package EscapeSquence;
+
+public class Escape {
+    public static void main(String[] args) {
+        System.out.println("Hello \"KGCoding\"...");
+        System.out.println("Hello \nKGCoding...");
+    }
+}
