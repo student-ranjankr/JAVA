481f7c539bc116cd822766e8eb384dc16e20a3cf
student-ranjankr <165934316+student-ranjankr@users.noreply.github.com>
2024-04-19 02:14:06 +0530

    Add files via upload

---
+++ basic_uses_of_functions.java
@@ -0,0 +1,42 @@
+package Functions;
+
+public class basic_uses_of_functions {
+    public static void main(String[] args)
+    {
+        System.out.println();
+        System.out.println(" basic_uses_of_functions ");
+        System.out.println();
+        function1();
+        System.out.println();
+        function2();
+        System.out.println();
+        function3();
+
+    }
+    public static void function1()
+    {
+
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        }
+
+    public  static void function2()
+    {
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+    public static void function3()
+    {
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
