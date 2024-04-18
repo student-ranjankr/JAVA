d747dfa50687a916aa3ee1c85d9ed766a8c8ced6
student-ranjankr <165934316+student-ranjankr@users.noreply.github.com>
2024-04-19 02:14:56 +0530

    Add files via upload

---
+++ syntax_of_functions.java
@@ -0,0 +1,16 @@
+package Functions;
+
+public class syntax_of_functions {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println(" Use of syntax_of_functions ");
+        System.out.println();
+        System.out.println(" In main method ");
+        greeting();
+        System.out.println(" Method calling complete ");
+    }
+    public  static void greeting()
+    {
+        System.out.println(" Good Morning from KGCoding ");
+    }
+}
